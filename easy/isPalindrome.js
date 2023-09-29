fcd865a1315719970446bdab5aab647437795210
Jakob <shavinski.jakob@gmail.com>
2023-09-29 15:19:25 -0700

    finish isPalindrome another way/method

--- easy/isPalindrome.js
+++ easy/isPalindrome.js
@@ -30,3 +30,63 @@ var isPalindrome = function (x) {
 
     return true;
 }
+
+/**
+ * @param {string} s
+ * @return {boolean}
+
+
+
+
+
+ANOTHER WAY 
+    create a helped function to determine if a char is alpha num
+        check if char is between a-z A-Z 0-9
+
+    in isPalindrome
+
+        left and right tracker 
+
+        need to do a while loop, while left is not equal to right
+            if the current char on left is not alpha num
+                add to one
+            if the current char on right is not alpha num
+                add one to right
+
+            if the char on left is not equal to char on right
+                return false
+
+    return true;
+ */
+function isPalindrome(s) {
+
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        while (left < right && !isValidChar(s[left])) {
+            left += 1;
+        }
+
+        while (left < right && !isValidChar(s[right])) {
+            right -= 1;
+        }
+
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+            return false;
+        }
+
+        left += 1;
+        right -= 1;
+    };
+
+    return true;
+};
+
+function isValidChar(char) {
+    return (
+        (char >= 'A' && char <= 'Z') ||
+        (char >= 'a' && char <= 'z') ||
+        (char >= '0' && char <= '9')
+    )
+}
