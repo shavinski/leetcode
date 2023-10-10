99439944e5e2b511e3e0eb3824629a9c7b7268a8
Jakob <shavinski.jakob@gmail.com>
2023-10-09 17:53:37 -0700

    Finish invertTree

---
+++ easy/trees/invertTree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+function invertTree(root) {
+    if (root === null) return root;
+
+    invertTree(root.left);
+    invertTree(root.right);
+
+
+    let temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+
+    return root;
+};
